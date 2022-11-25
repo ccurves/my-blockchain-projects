181976fffc89f825fa276cc3a17de2321d72d4aa
ccurves <simoncovenant47@gmail.com>
2022-11-25 01:39:37 +0100

    Worked on lottery deploy scripts

--- hardhat-lottery/deploy/01-deploy-lottery.js
+++ hardhat-lottery/deploy/01-deploy-lottery.js
@@ -1,12 +1,28 @@
-const { network } = require("hardhat");
+const { network, ethers } = require("hardhat");
+const {
+  developmentChains,
+  networkConfig,
+} = require("../helper-hardhat-config");
 
 module.exports = async ({ getNamedAccounts, deployments }) => {
   const { deploy, log } = deployments;
   const { deployer } = await getNamedAccounts();
+  const chainId = network.config.chainId;
+  let vrfCoordinatorV2Address;
 
+  if (developmentChains.includes(network.name)) {
+    const vrfV2Mock = await ethers.getContractAt("VRFCoordinatorV2Mock");
+    vrfCoordinatorV2Address - vrfV2Mock;
+  } else {
+    vrfCoordinatorV2Address = networkConfig[chainId]["VRFCoordinator"];
+  }
+
+  const entranceFee = networkConfig[chainId]["entranceFee"];
+  const gasLane = networkConfig[chainId]["gasLane"];
+  const args = [vrfCoordinatorV2Address, entranceFee, gasLane];
   const lottery = await deploy("Lottery", {
     from: deployer,
-    args: [],
+    args: args,
     log: true,
     waitConfirmations: network.config.blockConfirmations,
   });
--- hardhat-lottery/helper-hardhat-config.js
+++ hardhat-lottery/helper-hardhat-config.js
@@ -1,11 +1,15 @@
+const { ethers } = require("hardhat");
+
 const networkConfig = {
   5: {
     name: "goerli",
     VRFCoordinator: "0x2Ca8E0C643bDe4C2E08ab1fA0da3401AdAD7734D",
+    entranceFee: ethers.utils.parseEther("0.01"),
+    gasLane: "",
   },
-  137: {
-    name: "polygon",
-    ethUsdPriceFeed: "0x73366Fe0AA0Ded304479862808e02506FE556a98",
+  31337: {
+    name: "hardhat",
+    entranceFee: ethers.utils.parseEther("0.01"),
   },
 };
 
